05e899e51da0edeb74f3605ea6b590bf0a64a127
Nazwa8 <matiskowron2006@gmail.com>
2024-03-18 20:42:07 +0100

    Dodanie 3 przycisków

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun Witam(view: View) {
+
+        var displaytext : TextView = findViewById(R.id.textView)
+        displaytext.setText("Witam")
+    }
+
+    fun Mateusz(view: View) {
+
+        var displaytext: TextView = findViewById(R.id.textView2)
+        displaytext.setText("Mateusz")
+    }
+
+    fun Do_widzenia(view: View){
+
+        var displaytext: TextView = findViewById(R.id.textView3)
+        displaytext.setText("Do widzenia")
     }
 }
